be1bbbcf06d7295f0d176fd4af9d704e9eca571f
kreangkrai <kreangkrai@insightera.co.th>
2019-03-16 09:06:27 +0700

    add sample model

---
+++ chapter-gin/model/init.go
@@ -0,0 +1,17 @@
+package model
+
+import (
+	"github.com/jackc/pgx"
+	"log"
+)
+
+var PgxConn *pgx.Conn
+
+func init() {
+	config := pgx.ConnConfig{Host: "172.17.0.2", Port: 5432, Database: "dvdrental", User: "postgres", Password: "toor123"}
+	conn, err := pgx.Connect(config)
+	if err != nil {
+		log.Fatalf("Unable to establish connection: %v", err)
+	}
+	PgxConn = conn
+}
