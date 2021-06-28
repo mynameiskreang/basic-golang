095ca592f9ff2ec570596ebbe1d220fb22e1d730
Kreangkrai Supchid <kreangkrai.sup@gmail.com>
2021-06-28 10:24:12 +0700

    add evenDigitsOnly

--- code-signal/arcade/rains-of-reason.go
+++ code-signal/arcade/rains-of-reason.go
@@ -1,5 +1,7 @@
 package main
 
+import "strconv"
+
 // 25: arrayReplace https://app.codesignal.com/arcade/intro/level-6/mCkmbxdMsMTjBc3Bm
 func arrayReplace(inputArray []int, elemToReplace int, substitutionElem int) []int {
 	for i, a := range inputArray {
@@ -8,4 +10,22 @@ func arrayReplace(inputArray []int, elemToReplace int, substitutionElem int) []i
 		}
 	}
 	return inputArray
+}
+
+// 26: evenDigitsOnly https://app.codesignal.com/arcade/intro/level-6/6cmcmszJQr6GQzRwW
+func evenDigitsOnly(n int) bool {
+	mapOdd := map[string]bool{
+		"1": false,
+		"3": false,
+		"5": false,
+		"7": false,
+		"9": false,
+	}
+	nStr := strconv.Itoa(n)
+	for _, v := range nStr {
+		if b, exist := mapOdd[string(v)]; exist {
+			return b
+		}
+	}
+	return true
 }
