3e96ccca725fbecabd8ea9239a47dd2fe34a45b6
Kreangkrai Supchid <kreangkrai.sup@gmail.com>
2021-07-19 14:32:45 +0700

    add firstDigit,arrayMaxConsecutiveSum

--- code-signal/arcade/diving-deeper.go
+++ code-signal/arcade/diving-deeper.go
@@ -14,11 +14,50 @@ func extractEachKth(inputArray []int, k int) []int {
 	return outputArray
 }
 
-//35: differentSymbolsNaive https://app.codesignal.com/arcade/intro/level-8/8N7p3MqzGQg5vFJfZ
+//35: firstDigit https://app.codesignal.com/arcade/intro/level-8/rRGGbTtwZe2mA8Wov
+func firstDigit(inputString string) string {
+	mapChar := map[rune]bool{
+		'0': true,
+		'1': true,
+		'2': true,
+		'3': true,
+		'4': true,
+		'5': true,
+		'6': true,
+		'7': true,
+		'8': true,
+		'9': true,
+	}
+	for _, v := range inputString {
+		if mapChar[v] {
+			return string(v)
+		}
+	}
+	return ""
+}
+
+//36: differentSymbolsNaive https://app.codesignal.com/arcade/intro/level-8/8N7p3MqzGQg5vFJfZ
 func differentSymbolsNaive(s string) int {
 	mapSymbols := map[rune]bool{}
 	for _, v := range s {
 		mapSymbols[v] = true
 	}
 	return len(mapSymbols)
+}
+
+//37: arrayMaxConsecutiveSum https://app.codesignal.com/arcade/intro/level-8/Rqvw3daffNE7sT7d5
+func arrayMaxConsecutiveSum(inputArray []int, k int) int {
+	max, sum := inputArray[0], 0
+	for i := 0; i < len(inputArray); i++ {
+		if i < k {
+			sum += inputArray[i]
+		} else {
+			sum -= inputArray[i-k]
+			sum += inputArray[i]
+		}
+		if sum > max {
+			max = sum
+		}
+	}
+	return max
 }
