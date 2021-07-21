06cdfaa3f176fb18dfc573d8b8103454bab4de56
Kreangkrai Supchid <kreangkrai.sup@gmail.com>
2021-07-21 09:33:55 +0700

    add knapsackLight

--- code-signal/arcade/dark-wilderness.go
+++ code-signal/arcade/dark-wilderness.go
@@ -11,3 +11,30 @@ func growingPlant(upSpeed int, downSpeed int, desiredHeight int) int {
 	}
 	return output + 1
 }
+
+//39: knapsackLight https://app.codesignal.com/arcade/intro/level-9/r9azLYp2BDZPyzaG2
+func knapsackLight(value1 int, weight1 int, value2 int, weight2 int, maxW int) int {
+	// Step1 order
+	orderValue := make([]*int, 2)
+	orderWeight := make([]*int, 2)
+	if value1 > value2 {
+		orderValue[0] = &value1
+		orderValue[1] = &value2
+		orderWeight[0] = &weight1
+		orderWeight[1] = &weight2
+	} else {
+		orderValue[0] = &value2
+		orderValue[1] = &value1
+		orderWeight[0] = &weight2
+		orderWeight[1] = &weight1
+	}
+	// Step2 calculate
+	sum := 0
+	for i, _ := range orderValue {
+		if maxW-*orderWeight[i] >= 0 {
+			maxW = maxW - *orderWeight[i]
+			sum += *orderValue[i]
+		}
+	}
+	return sum
+}
