f69a066c948562f9029227efdb8cb8a986676b40
Kreangkrai Supchid <kreangkrai.sup@gmail.com>
2021-07-30 14:30:13 +0700

    add countriesSelection

--- code-signal/database/welcome-to-the-table.sql
+++ code-signal/database/welcome-to-the-table.sql
@@ -3,3 +3,9 @@ CREATE PROCEDURE projectList()
 BEGIN
 select project_name,team_lead,income FROM Projects order by internal_id ASC limit 10;
 END
+
+-- 2: countriesSelection https://app.codesignal.com/arcade/db/welcome-to-the-table/jLeSZGMvaEhSJnEsS
+CREATE PROCEDURE countriesSelection()
+BEGIN
+select * from countries where continent = 'Africa';
+END
