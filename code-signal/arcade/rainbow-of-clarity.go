5d915a7e1851cf12ca480b6e14d866cd52ca3ec7
kreangkrai supchid <kreangkrai.sup@gmail.com>
2021-08-23 09:41:40 +0700

    add lineEncoding

--- code-signal/arcade/rainbow-of-clarity.go
+++ code-signal/arcade/rainbow-of-clarity.go
@@ -1,8 +1,38 @@
 package main
 
-import "unicode"
+import (
+	"fmt"
+	"unicode"
+)
 
-//48: isDigit https://app.codesignal.com/arcade/intro/level-11/Zr2XXEpkBPtYWqPJu/import "unicode"
+//48: isDigit https://app.codesignal.com/arcade/intro/level-11/Zr2XXEpkBPtYWqPJu
 func isDigit(symbol string) bool {
 	return unicode.IsDigit(rune(symbol[0]))
+}
+
+//49: lineEncoding https://app.codesignal.com/arcade/intro/level-11/o2uq6eTuvk7atGadR
+func lineEncoding(s string) string {
+	checker := rune(s[0])
+	result := ""
+	count := 0
+	for _, r := range s {
+		if checker == r {
+			count++
+			continue
+		} else {
+			if count > 1 {
+				result = result + fmt.Sprintf("%d%c", count, checker)
+			} else {
+				result = result + fmt.Sprintf("%c", checker)
+			}
+			count = 1
+			checker = r
+		}
+	}
+	if count > 1 {
+		result = result + fmt.Sprintf("%d%c", count, checker)
+	} else {
+		result = result + fmt.Sprintf("%c", checker)
+	}
+	return result
 }
