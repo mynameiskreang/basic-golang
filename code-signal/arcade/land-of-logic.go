fbbf2c3a5e3061199efb39377fa1c29b5bc599bf
kreangkrai supchid <kreangkrai.sup@gmail.com>
2021-09-02 09:15:14 +0700

    add go validTime

--- code-signal/arcade/land-of-logic.go
+++ code-signal/arcade/land-of-logic.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 )
 
 // 52: longestWord https://app.codesignal.com/arcade/intro/level-12/s9qvXv4yTaWg8g4ma
@@ -34,4 +35,20 @@ func longestWord(text string) string {
 		ans = tmp
 	}
 	return ans
+}
+
+// 53: validTime https://app.codesignal.com/arcade/intro/level-12/ywMyCTspqGXPWRZx5
+func validTime(time string) bool {
+	v1, _ := strconv.Atoi(string(time[:2]))
+	v2, _ := strconv.Atoi(string(time[3:]))
+	if v1 >= 24 {
+		return false
+	}
+	if v2 > 60 {
+		return false
+	}
+	if v1 == 0 && v2 == 60 {
+		return false
+	}
+	return true
 }
